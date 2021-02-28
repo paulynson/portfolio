2172d2399122039c8cecf2bf593a5b62b540c0a0
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-02-28 15:27:34 +0100

    404 page

--- src/pages/NoActivePage.js
+++ src/pages/NoActivePage.js
@@ -15,17 +15,17 @@ function NoActivePage() {
            <div class="div2"></div>
     {/* <!-- end of the div animation --> */}
 
-            <h1 className="text-center">404</h1>
-            <h3 className="text-dark text-uppercase">PAGE NOT FOUND</h3>
+            <h1 className="text-center" data-aos="fade-right" data-aos-duration="1000">404</h1>
+            <h3 className="text-dark text-uppercase" data-aos="fade-down" data-aos-duration="1000">PAGE NOT FOUND</h3>
 
-            <div className="mt-3 d-flex justify-content-center text-white">
+            <div className="mt-3 d-flex justify-content-center text-white" data-aos="fade-up" data-aos-duration="1000">
 <a href="https://www.facebook.com/Abakadapaul/" target="-blank" rel="noreferrer" className="text-dark px-3"> <FaFacebook /> </a> 
 <a href="https://twitter.com/Abakadapaul/" target="-blank" rel="noreferrer" className="text-dark px-3"> <FaTwitter /> </a>
 <a href="https://instagram.com/Abakadapaul/" target="-blank" rel="noreferrer"  className="text-dark px-3"> <FaInstagram /> </a> 
 <a href="https://github.com/paulynson" target="-blank" rel="noreferrer" className="text-dark px-3"> <FaGithub /> </a> 
   </div>
 
-  <div className="mt-5 display-5">
+  <div className="mt-5 display-5" data-aos="fade-down" data-aos-duration="1000">
 
 <Link to="/" className=" btn btn-dark"> Back Home </Link> 
 </div>
