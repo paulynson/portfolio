359daaf61634a364b6f0ca53c89dc8347b72c230
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-01-14 10:09:52 +0100

    Mapping through the listed items

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import about from '../assets/images/about.png';
-import { FaArrowDown } from 'react-icons/fa';
+import About from './About';
 
 // import wk1 from "./images/wk1.PNG"
 
 function Home() {
- 
   const workDetails = [
     {
       id: 1,
       link: 'http://mobiletoiletsystems.com.ng',
-      pix: require ('./images/wk1.PNG').default,
+      pix: require('./images/wk1.PNG').default,
       work: 'Mobile Toilet Systems',
       tools: 'WORDPRESS',
       info: 'Mobile Toilet Website',
@@ -20,7 +18,7 @@ function Home() {
     {
       id: 2,
       link: 'https://barber-shop-envato.netlify.app',
-      pix: require ('./images/wk2.PNG').default,
+      pix: require('./images/wk2.PNG').default,
       work: 'Barber Shop Website',
       tools: 'HTML5, CSS3, BOOTSTRAP',
       info: 'Barber-shop website',
@@ -29,7 +27,7 @@ function Home() {
     {
       id: 3,
       link: 'https://platinumhotels.netlify.app',
-      pix: require ('./images/wk3.PNG').default,
+      pix: require('./images/wk3.PNG').default,
       work: 'Platinum Hotel Website',
       tools: 'HTML5, CSS3, BOOTSTRAP, JAVASCRIPT',
       info: 'Platinum Hotel',
@@ -38,7 +36,7 @@ function Home() {
     {
       id: 4,
       link: 'https://doggy-villa.netlify.app/',
-      pix: require ('./images/wk4.png').default,
+      pix: require('./images/wk4.png').default,
       work: 'Dogg-Villa Care',
       tools: 'HTML5, CSS3, JAVASCRIPT',
       info: 'Dog Care Website',
@@ -47,7 +45,7 @@ function Home() {
     {
       id: 5,
       link: 'https://mobilescreen.netlify.app',
-      pix: require ('./images/wk5.PNG').default,
+      pix: require('./images/wk5.PNG').default,
       work: 'Mobile Viewport Design',
       tools: 'HTML5, CSS3, BOOTSTRAP, JAVASCRIPT',
       info: 'Mobile viewport',
@@ -56,7 +54,7 @@ function Home() {
     {
       id: 6,
       link: 'http://leisure-lawn.com',
-      pix: require ('./images/wk6.PNG').default,
+      pix: require('./images/wk6.PNG').default,
       work: 'Leisure Lawn Business Website',
       tools: 'HTML5, CSS3, BOOTSTRAP, JAVASCRIPT',
       info: 'Leisure-Lawn Website',
@@ -107,190 +105,29 @@ function Home() {
       <main className="container my-5">
         {/* About Section */}
 
-        <section>
-          <div className="bg-light my-5 py-5 px-5">
-            <div className="row d-flex">
-              <div className="container col-lg-4 text-lg-left order-sm-2 order-md-2 order-lg-1">
-                <h2
-                  className="text-dark font-weight-bolder mt-md-5 mt-sm-5"
-                  data-aos="fade-down"
-                  data-aos-duration="1000"
-                >
-                  About me
-                </h2>
-                <h5
-                  data-aos="fade-right"
-                  data-aos-duration="1000"
-                  data-aos-once="true"
-                >
-                  I'm a Frontend Developer and a UI designer based in Lagos
-                  State, Nigeria.
-                </h5>
-                <small>
-                  <p
-                    className="text-muted text-lg-left mt-4"
-                    data-aos="fade-down"
-                    data-aos-duration="1000"
-                    data-aos-once="true"
-                  >
-                    I love solving complex problems that gives me more idea
-                    turning it to something beautiful and eye catching which
-                    also gives me the innovation to work with pixels as a great
-                    tool of beauty. When I'm not coding, I love playing football
-                    or video games.
-                  </p>
-                </small>
-                {/* <code>I use this for practice</code> */}
-              </div>
-
-              <div
-                className="col-lg-6 text-center order-sm-1 order-md-1 order-lg-2"
-                data-aos="fade-up"
-                data-aos-duration="1000"
-              >
-                <img
-                  src={about}
-                  className="img-fluid w-75 rounded-circle border border-light"
-                  alt="about-img"
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* Bootstrap Icon Arrow */}
-
-          <div className="container d-flex justify-content-center text-center text-white">
-            <FaArrowDown className="blink-btn" />
-          </div>
-
-          <div className="container my-5">
-            <div className="row">
-              <div
-                className="col-lg-6"
-                data-aos="fade-up"
-                data-aos-duration="1000"
-              >
-                <div className="shadow py-5 px-5">
-                  <h3
-                    className="text-center font-weight-bold"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                  >
-                    Front-end Developer
-                  </h3>
-                  <p
-                    className="text-center mt-3"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                  >
-                    I like to code things from scratch, and enjoy bringing ideas
-                    to life in the browser.
-                  </p>
-
-                  <h4
-                    className="text-primary mt-5"
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                  >
-                    Languages I Speak:
-                  </h4>
-                  <p data-aos="fade-right" data-aos-duration="1000">
-                    {' '}
-                    HTML5, Bootstrap, Tailwind, CSS3, SASS, JavaScript, React Js{' '}
-                  </p>
-
-                  <h4 className="text-primary mt-5">Dev Tools:</h4>
-                  <ul className="list-group">
-                    <li className="list-group-item">VScode</li>
-                    <li className="list-group-item">
-                      Google Chrome Inspect Tools
-                    </li>
-                    <li className="list-group-item">GitHub</li>
-                    <li className="list-group-item">Netlify</li>
-                    <li className="list-group-item">CodeSandBox</li>
-                  </ul>
-                </div>
-              </div>
-
-              {/* Design Aspect */}
-
-              <div
-                className="col-lg-6"
-                data-aos="fade-down"
-                data-aos-duration="1000"
-              >
-                <div className="shadow py-5 px-5">
-                  <h3
-                    className="text-center font-weight-bold"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                  >
-                    Designer
-                  </h3>
-                  <p
-                    className="text-center mt-3"
-                    data-aos="fade-right"
-                    data-aos-duration="1000"
-                  >
-                    I value simple content structure, clean design patterns, and
-                    thoughtful interactions.
-                  </p>
-
-                  <h4
-                    className="text-primary mt-5"
-                    data-aos="fade-down"
-                    data-aos-duration="1000"
-                  >
-                    Things I enjoy designing:
-                  </h4>
-                  <p data-aos="fade-up" data-aos-duration="1000">
-                    {' '}
-                    UI, Web, Mobile, Logos{' '}
-                  </p>
-
-                  <h4 className="text-primary mt-5">Design Tools:</h4>
-                  <ul className="list-group">
-                    <li className="list-group-item">Figma</li>
-                    <li className="list-group-item">Adobe XD</li>
-                    <li className="list-group-item">Adobe PhotoShop</li>
-                    <li className="list-group-item">CorelDraw</li>
-                    <li className="list-group-item">WhiteBoard Animation</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="text-center my-5 h-50" id="portfolio"></div>
-        </section>
+        <About />
 
         {/* About Section */}
 
         {/* Portfolio */}
-<hr />
-        <div className="my-5">
+        <hr />
+        <section className="my-5">
           <h2 className="h2 mt-5">
             {' '}
             Interested in collaborating or investing?
           </h2>
 
-          <h6
-            className="my-3"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-          >
-            I’m always open to discussing Web Development, Product Design, work
+          <h6 className="my-3" data-aos="fade-up" data-aos-duration="1000">
+            I'm always open to discussing Web Development, Product Design, work
             or partnership opportunities.
           </h6>
-          <Link
-            className="my-2 btn btn-outline-dark"
-            to="/contact"
-          >
+          <Link className="my-2 btn btn-outline-dark" to="/contact">
             Start a Conversation
           </Link>
-        </div>
-<hr />
+        </section>
+        <hr />
         {/* mapping work done */}
-        <div className='my-5'>
+        <section className="my-5">
           <h2 className="h2 mt-5">PORTFOLIO</h2>
 
           <h6
@@ -336,7 +173,7 @@ function Home() {
               </div>
             ))}
           </div>
-        </div>
+        </section>
         {/* mapping work done */}
       </main>
     </div>
